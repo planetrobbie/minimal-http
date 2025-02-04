1b2423d48482f27570ee09ca21159d0652cf4eb9
Sébastien Braun <sebbraun@gmail.com>
2025-02-04 12:12:12 +0100

    listen on 127.0.0.1 to avoid annoying macos popups

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 	http.HandleFunc("/", handleFunc)
 	fmt.Println("Server starting on Port :3000")
 
-	// on 127.0.0.1 to avoid macos popup asking for authorisation
+	// listen on 127.0.0.1 to avoid macos popup asking for auth
 	// otherwise sign your binary: codesign -s - <binary>
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe("127.0.0.1:3000", nil)
 
 	// checking errors isn't stricly necessary at main end
 	// added just for learning purpose
